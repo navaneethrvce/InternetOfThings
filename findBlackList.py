cb7ff4a4e9586fa13e0925e3a54fee09683d36f1
shahkaushal94 <shahkaushal94@gmail.com>
2017-03-14 15:10:43 -0700

    Correct file name

--- findBlackList.py
+++ findBlackList.py
@@ -18,14 +18,14 @@ import requests,json
 from bs4 import BeautifulSoup
 
 r = requests.post('http://www.ipvoid.com/ip-blacklist-check/', data = {'ip':'204.140.180.2'})
+print r
+# soup = BeautifulSoup(r,"html.parser")
+# table = soup.find("table", attrs= {"class" : "label label-success"})
+# headings = [th.get_text() for th in table.find("tr").find_all("th")]
 
-soup = BeautifulSoup(r,"html.parser")
-table = soup.find("table", attrs= {"class" : "label label-success"})
-headings = [th.get_text() for th in table.find("tr").find_all("th")]
+# datasets = []
+# for row in table.find_all("tr")[1:]:
+#     dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
+#     datasets.append(dataset)
 
-datasets = []
-for row in table.find_all("tr")[1:]:
-    dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
-    datasets.append(dataset)
-
-print datasets
+# print datasets
