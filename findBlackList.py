5fa0ad7f5516ea51d13a1e2900b734310395fad1
shahkaushal94 <shahkaushal94@gmail.com>
2017-03-14 16:42:50 -0700

    Correct file name

--- findBlackList.py
+++ findBlackList.py
@@ -1,31 +1,21 @@
-# from urllib import urlopen
-# story = urlopen('https://www.python.org')
-# story_words = []
-# file  = open("output.txt","w")
-
-# for line in story:
-# 	file.write(story_words)
-
-
-# for line in story:
-#     line_words = line.split()
-#     for words in line_words:
-#         story_words.append(words)
-
-#     file.write(story_words)
-
-import requests,json
+import requests
+import json
 from bs4 import BeautifulSoup
 
+
+file  = open("findBlackList.txt","a+")
+
 r = requests.post('http://www.ipvoid.com/ip-blacklist-check/', data = {'ip':'204.140.180.2'})
-print r
-# soup = BeautifulSoup(r,"html.parser")
-# table = soup.find("table", attrs= {"class" : "label label-success"})
-# headings = [th.get_text() for th in table.find("tr").find_all("th")]
 
-# datasets = []
-# for row in table.find_all("tr")[1:]:
-#     dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
-#     datasets.append(dataset)
+#print r.text
+soup = BeautifulSoup(r.text,"html.parser")
+table = soup.find("table", attrs= {"class" : "table table-striped table-bordered"})
+headings = [th.get_text() for th in table.find("tr").find_all("td")]
 
-# print datasets
+datasets = []
+for row in table.find_all("tr")[1:]:
+    dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
+    file.write(str(dataset))
+    datasets.append(dataset)
+
+print datasets
