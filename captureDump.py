1c170770d35bd2970f24491dfb1b04a1f2d2af0e
NaikPrats <prathamesh.naik20@gmail.com>
2017-03-14 18:23:13 -0700

    Add files via upload
    Capture tcpdump and output result into result.txt

---
+++ captureDump.py
@@ -0,0 +1,19 @@
+import os
+
+fw = open('result.txt',"w")
+
+while(1):
+	result = os.popen("sudo tcpdump -n -i eth0 -c 1 | awk '{print $1,$2,$3,$5}' ").read()
+	token = result.split()
+	time,proto,source,dest = token[0],token[1],token[2],token[3]
+	index = source.rfind('.')
+	srcPort = source[index+1:]
+	source = source[:index]
+	index = dest.rfind('.')
+	dstPort = dest[index+1:]
+	dest = dest[:index]
+	dstPort = dstPort[:-1]
+	print 'source :',source,'srcPort',srcPort,'Dest',dest,'DestPort',dstPort
+	fw.write(source+','+srcPort+','+dest+','+dstPort+','+time+','+proto+'\n')
+
+fw.close()
