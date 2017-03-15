28a3865ae0d4609d17800b1353e25c51724c95ce
NaikPrats <prathamesh.naik20@gmail.com>
2017-03-15 10:58:41 -0700

    Add files via upload

--- captureDump.py
+++ captureDump.py
@@ -3,11 +3,13 @@ import os
 fw = open('result.txt',"w")
 
 while(1):
-	result = os.popen("sudo tcpdump -nn -i wlp0s20u12 -c 1 | awk '{print $1,$2,$3,$5}' ").read()
+	result = os.popen("sudo tcpdump -n -i eth0 not arp and not rarp -c 1 | awk '{print $1,$2,$3,$5}' ").read()
 	token = result.split()
 	time,proto,source,dest = token[0],token[1],token[2],token[3]
+	print source
 	index = source.rfind('.')
 	srcPort = source[index+1:]
+	print srcPort
 	source = source[:index]
 	index = dest.rfind('.')
 	dstPort = dest[index+1:]
