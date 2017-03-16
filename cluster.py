0b52061496f2107cc5a01c1b454d409b4cf97904
Karthik C S <cskarthik02@gmail.com>
2017-03-15 17:59:36 -0700

    initial commit: cluster.py

--- cluster.py
+++ cluster.py
@@ -1,66 +1,35 @@
-
 home_appliances=['coffee maker','watering controller','sprinkler controller','plant monitor','automatic feeder','precision cooker','oven','grill thermometer']
 
 entertainment=['wristband','wireless speakers','tv','television']
 
-heatlth_fitness=['activity tracker','yoga mat','thermometer','blood pressure monitor','fitbit','sleep tracker','heart rate monitoring']
+health_fitness=['activity tracker','yoga mat','thermometer','blood pressure monitor','fitbit','sleep tracker','heart rate monitoring']
 
 general_purpose=['kindle','laptops','computer','desktop','mobile','cellphones']
 
-
-a=['Samsung']
-
-def categorize(dns, dest_ip_count, dest_port):
-    for index,x in enumerate(a):
+def categorize(dnss, dest_ip_count):
+    dns = dnss.split()
+    for index,x in enumerate(dns):
         dns[index]=x.lower()
-#print 'snxkaan'
-#print a
-    for i in a:
+
+    for i in dns:
         print i
         if i in home_appliances:
-            print 'I am home app'
             return 'home'
         elif i in entertainment:
-            print 'I am enter'
             return 'enter'
-        elif i in heatlth_fitness:
-            print 'I am health'
+        elif i in health_fitness:
             return 'health'
         elif i in general_purpose:
-            print 'I am general'
             return 'general'
 
-    for i in a:
-        if dest_ip_count < 50:
-            print 'I am health'
-            return 'health'
-        elif dest_ip_count < 100:
-            print 'I am home app'
-            return 'home'
-        elif dest_ip_count < 1000:
-            print 'I am enter'
-            return 'enter'
-        else:
-            print 'I am general'
-            return 'general'
-
-    #input is a list of destination ports
-
-    heavily_used_port=max(set(dest_port), key=lst.count)
-    if (heavily_used_port== || heavily_used_port ==):
-        print 'I am health'
+    if dest_ip_count < 50:
         return 'health'
-    elif (heavily_used_port == || heavily_used_port ==):
-         print 'I am home app'
-         return 'home'
-    elif (heavily_used_port == || heavily_used_port ==):
-        print 'I am enter'
+    elif dest_ip_count < 100:
+        return 'home'
+    elif dest_ip_count < 1000:
         return 'enter'
     else:
-        print 'I am general'
         return 'general'
 
 
-
-
-#b=[samsung,smart,tv,hd,hdquality,sony,lg,panasonic,vizio,toshiba,sharp,philips,Mmitsubishi,hisense,television,google,googletelevision,]
+print categorize(["samsung"], 100)
